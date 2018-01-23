096202e241347d31c2887fe76b32adc0f66e3bee
Tyson Hood <TysonHood@gmail.com>
2018-01-23 10:30:16 -0800

    First commit.

---
+++ main.js
@@ -0,0 +1,11 @@
+var obj1 = {
+	name_first: "Tyson",
+	name_last: "Hood",
+	email: "Tyson@TysonHood.com",
+	message: "Hi, I'm Tyson."
+}
+
+console.log(obj1);
+console.log(obj1.name_last);
+console.log(obj1.name_first);
+console.log(obj1.email);
