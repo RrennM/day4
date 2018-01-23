5f0fc8a7cf48451ece5045bb74238199cdb014fa
Tyson Hood <TysonHood@gmail.com>
2018-01-23 10:44:54 -0800

    Array Practice.

--- main.js
+++ main.js
@@ -5,7 +5,31 @@ var obj1 = {
 	message: "Hi, I'm Tyson."
 }
 
+var obj2 = {
+	name_first: "Sam",
+	name_last: "Smith",
+	email: "Sam@TysonHood.com",
+	message: "Hi, I'm Sam."
+}
+
 console.log(obj1);
 console.log(obj1.name_last);
 console.log(obj1.name_first);
 console.log(obj1.email);
+
+var arr1 = [ "Tyson" , "Hood" , "Tyson@TysonHood.com" , "Hi, I'm Tyson." ]
+console.log(arr1);
+console.log(arr1[2]);
+
+var arr1 = [ obj1.name_first , obj1.name_last , obj1.email , obj1.message ]
+console.log(arr1);
+console.log(arr1[2]);
+
+var arr1 = [ obj2.name_first , obj2.name_last , obj2.email , obj2.message ]
+console.log(arr1);
+console.log(arr1[2]);
+
+
+var arr1 = [ obj1 , obj2 ]
+console.log(arr1);
+console.log(arr1[1]);
